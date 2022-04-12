03eb1ad39c6b704c45eef0b0ddc9e8b889a1e080
Emil Gelev <emil.gelev@gmail.com>
2022-04-12 12:24:52 +0300

    Spring Initializer

---
+++ src/main/java/com/egelev/nra/NraApplication.java
@@ -0,0 +1,13 @@
+package com.egelev.nra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egelev/nra/NraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egelev.nra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
